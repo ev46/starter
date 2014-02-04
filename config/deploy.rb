f03e7fcc7faf8ada9b9797bde74ba693c238874a
ev46 <ev46@cornell.edu>
2014-02-04 14:17:00 -0500

    changing deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-require 'mina/rvm'    # for rvm support. (http://rvm.io)
+# require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -10,26 +10,18 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :app, 'activemenu'
-set :domain, 'activemenu.org'
-set :deploy_to, '/home/deploy/activemenu'
-set :repository, 'git@github.com:ev46/starter.git'
+set :domain, 'foobar.com'
+set :deploy_to, '/var/www/foobar.com'
+set :repository, 'git://...'
 set :branch, 'master'
 
-set :user, 'deploy'
-set :port, '22 -A'
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'log']
-set :rvm_path, '/usr/local/rvm/bin/rvm'
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
-
-# set :app_path, "#{deploy_to}/#{current_path}"
-# set :unicorn_config, "#{app_path}/config/unicorn.rb"
-# set :unicorn_pid, "#{app_path}/tmp/pids/unicorn.pid"
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
@@ -39,7 +31,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[ruby-2.0.0-p353@default]'
+  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -49,17 +41,12 @@ task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
 
-  # queue! %[mkdir -p "#{deploy_to}/shared/tmp/{pids,sockets}"]
-  # queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp/{pids,sockets}"]
-
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
 
   queue! %[touch "#{deploy_to}/shared/config/database.yml"]
   queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
 end
-
-
 
 desc "Deploys the current version to the server."
 task :deploy => :environment do
@@ -71,18 +58,12 @@ task :deploy => :environment do
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
-    #invoke :'unicorn_and_nginx'
 
     to :launch do
-      #queue "touch #{deploy_to}/tmp/restart.txt"
-      #invoke :'unicorn:restart'
-      queue "restart activemenu"
+      queue "touch #{deploy_to}/tmp/restart.txt"
     end
   end
 end
-
-
-
 
 # For help in making your deploy script, see the Mina documentation:
 #
